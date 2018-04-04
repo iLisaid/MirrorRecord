ebefbde6c644634ff9cf788732392adb3806de70
iLisaid <iLisaid@users.noreply.github.com>
2018-04-04 12:41:21 +0300

    Paths

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        MirrorRecorder.record("E:\\WORK\\PRACTICE\\MirrorRecord\\fileIn", "E:\\WORK\\PRACTICE\\MirrorRecord\\fileOut");
+        MirrorRecorder.record(".\\fileIn", ".\\fileOut");
     }
 }
